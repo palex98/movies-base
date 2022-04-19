294cf835f4585e52915c92b1895451655cbbf681
Rostyslav <lysenkor@mydigicode.com>
2022-04-19 12:46:03 +0300

    add getMovieInfo method

--- src/movie/movie.service.ts
+++ src/movie/movie.service.ts
@@ -1,4 +1,9 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class MovieService {}
+export class MovieService {
+  getMovieInfo(titleName: string): any {
+    const movieName = titleName.trim();
+    return `http://www.omdbapi.com/?t=${movieName}&apikey=b53c371c`;
+  }
+}
