07f6f8e4ecd6143821290506dc7575cbaf3063ff
Rostyslav <lysenkor@mydigicode.com>
2022-04-19 11:41:49 +0300

    add class getMovieInfoDto

---
+++ src/movie/dto/getMovieInfo.dto.ts
@@ -0,0 +1,14 @@
+export class getMovieInfoDto {
+  readonly Title: string;
+  readonly Year: string;
+  readonly Released: string;
+  readonly Runtime: string;
+  readonly Genre: string;
+  readonly Director: string;
+  readonly Actors: string;
+  readonly Plot: string;
+  readonly Language: string;
+  readonly Country: string;
+  readonly Poster: string;
+  readonly imdbRating: string;
+}
