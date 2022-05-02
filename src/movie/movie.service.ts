e649603472203e2b6c3fd1c9ec022919e7f0d6fa
Rostyslav Lysenko <lysenkor@digicode.com>
2022-05-02 20:41:04 +0300

    Hid api key in ENV

--- src/movie/movie.service.ts
+++ src/movie/movie.service.ts
@@ -2,13 +2,13 @@ import { Injectable } from '@nestjs/common';
 import { HttpService } from '@nestjs/axios';
 import { firstValueFrom } from 'rxjs';
 import { GetMovieInfoDto } from './dto/getMovieInfo.dto';
+
 @Injectable()
 export class MovieService {
   constructor(private httpService: HttpService) { }
   async getMovieInfo(title: string): Promise<GetMovieInfoDto> {
-    const key = process.env.API_KEY;
     const { data } = await firstValueFrom(
-      this.httpService.get(`/?t=${title}&apikey=${key}`),
+      this.httpService.get(`/?t=${title}&apikey=${process.env.API_KEY}`),
     );
     return data;
   }
